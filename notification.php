255e4d0c51aa7d87bbcfebd4eecf939c1b72aa31
Joaquin Sanchez <sanchezjoaquin1995@gmail.com>
2021-04-23 00:48:57 -0300

    Imprime raw body

--- notification.php
+++ notification.php
@@ -1,5 +1,6 @@
 <?php
-error_log('Request: ' . json_encode($_GET) . ' ' . json_encode($_POST));
+$data = file_get_contents('php://input');
+error_log('Request: ' . json_encode($_GET) . ' ' . json_encode($_POST) . ' ' . $data);
 
 http_response_code(200);
 ?>
